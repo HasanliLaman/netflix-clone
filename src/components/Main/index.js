c382924caf4485b50fd1b4af62b8caea47f1ec75
“Laman <hesenlileman2001@gmail.com>
2022-08-08 15:19:43 +0400

    Remove unused imports

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import MainCategories from "./MainCategories";
 import MainHero from "./MainHero";
 import MainContainer from "./style";
-import useFetch from "../../hooks/useFetch";
 
 const Main = () => {
   const [id, setId] = useState("");
